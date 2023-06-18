6a212d225438187e828c123ca8df7a9f0337d2c5
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-06-19 00:57:46 +0300

    installed supports-color

--- app/content.tsx
+++ app/content.tsx
@@ -12,7 +12,7 @@ const Content = ({ shows }: any) => {
 			{shows.length > 0 && <Slider shows={shows} activeSlider={activeSlider} setActiveSlider={setActiveSlider} setWatchTrailer={setWatchTrailer} />}
 			<h1 className="text-4xl font-semibold mt-6">Popular on {appName}</h1>
 			{/* Grid Shows */}
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 gap-6">{shows.map((show: any, idx: number) => <ShowCard key={idx} {...show} />).slice(0, 8)}</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 gap-6">{shows && shows.map((show: any, idx: number) => <ShowCard key={idx} {...show} />).slice(0, 8)}</div>
 			{watchTrailer && <WatchTrailer url={shows[activeSlider].trailer_link} setWatchTrailer={setWatchTrailer} />}
 		</main>
 	);
