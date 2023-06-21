3d469aeb6652cbfc631939c10215b9c366c6b9c2
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-06-21 18:39:16 +0300

    Again for Vercel

--- app/content.tsx
+++ app/content.tsx
@@ -11,6 +11,10 @@ const Content = ({ shows }: any) => {
 	return (
 		<main className="container py-4 mt-6">
 			{shows.length > 0 && <Slider shows={shows} activeSlider={activeSlider} setActiveSlider={setActiveSlider} setWatchTrailer={setWatchTrailer} />}
+			<h1 className="text-4xl font-semibold mt-6">Popular on {appName}</h1>
+			{/* Grid Shows */}
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 gap-6">{shows && shows.length > 0 && shows.map((show: any, idx: number) => <ShowCard key={idx} {...show} />).slice(0, 8)}</div>
+
 		</main>
 	);
 };
