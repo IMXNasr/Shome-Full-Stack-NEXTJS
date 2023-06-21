d4910d3a151a079570763cafd2c5e410defc8763
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-06-21 19:18:18 +0300

    Added show type page

--- app/[type]/content.tsx
+++ app/[type]/content.tsx
@@ -5,6 +5,7 @@ import { useParams } from "next/navigation";
 
 const ShowsContent = ({ shows }: any) => {
 	const type: any = useParams().type;
+	console.log(shows);
 	return (
 		<>
 			<main className="container py-4 mt-6">
