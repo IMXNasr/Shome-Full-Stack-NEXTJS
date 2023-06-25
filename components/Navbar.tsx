f61e735fdff607e2f01405606fb7e091e111a221
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-06-25 03:34:05 +0300

    Added Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,8 @@ import { getServerSession } from "next-auth";
 import NavbarContent from "./NavbarContent";
 
 const Navbar = async () => {
-  const session = await getServerSession(authOptions);
-  return (
-    <NavbarContent user={session && session.user} />
-  );
-}
+	const session = await getServerSession(authOptions);
+	return <NavbarContent user={session && session.user} />;
+};
 
 export default Navbar;
