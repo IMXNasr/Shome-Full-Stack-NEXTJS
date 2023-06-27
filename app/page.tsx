e1006840ff960d62ba6483e81dd2bd6c2a686798
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-06-27 08:04:21 +0300

    Added Navbar 0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "@/components";
 
-const HomePage = async () => {
+const HomePage = () => {
 	return (
 		<>
 			<Navbar />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,11 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
-import NavbarContent from "./NavbarContent";
 
 const Navbar = async () => {
 	const session = await getServerSession(authOptions);
 	return (
 		<>
+			<h1>Hi</h1>
 			{/* <NavbarContent user={session && session.user} /> */}
 		</>
 	);
