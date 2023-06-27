7f90f9920c7a0e027b82eb47f16c97882798acd8
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-06-27 08:37:01 +0300

    Added Navbar 0.1

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../[...nextauth]/route";
+import authOptions from "@/utils/auth";
 import { NextResponse } from "next/server";
 
 export const GET = async (request: Request) => {
