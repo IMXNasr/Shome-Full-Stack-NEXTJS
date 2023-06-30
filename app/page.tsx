ddd4d4890c442f962fd650b4a7c5cd2d2b8ab838
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-07-01 02:59:01 +0300

    Fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import HomeContent from "./content";
 
 const HomePage = async () => {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/shows/featured`);
-	const data = await res.json();
-
+	const data = await res.text();
+	console.log(data);
 	return (
 		<>
-			{res.status !== 200 ? <h1>{data}</h1> : <HomeContent shows={data} />}
+			{res.status !== 200 ? <h1>{data}</h1> : <HomeContent shows={JSON.parse(data)} />}
 		</>
 	);
 };
