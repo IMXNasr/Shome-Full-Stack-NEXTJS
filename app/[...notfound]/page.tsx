6aa07b413aea2370a618a9ed6707536a1e1cc82f
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-07-09 09:55:56 +0300

    Added not-found.tsx page

--- app/[...notfound]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { notFound } from "next/navigation";
-
-const NotFoundPage = () => {
-  return (
-    <div className="container">go go go  errrrorr</div>
-  )
-};
-
-export default NotFoundPage;
