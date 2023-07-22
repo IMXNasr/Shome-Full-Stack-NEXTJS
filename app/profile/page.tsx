13482192da37ce89ca613d0be81ee41824869e3f
Mahmoud Nasr <72768187+IMXMaNasr@users.noreply.github.com>
2023-07-22 11:54:23 +0300

    Fixed a issue in profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,8 @@ import { getTitle } from "@/utils/functions";
 import { notFound, redirect } from "next/navigation";
 import ProfileContent from "./content";
 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
 	title: getTitle("Profile"),
 };
